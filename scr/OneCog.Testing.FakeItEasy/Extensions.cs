1f2057f621859d0d29ab5c0a9b801e4f3d5eeb0d
ibebbs <github@bebbs.co.uk>
2015-05-14 08:32:55 +0100

    Commit of Testing.FakeItEasy and consolidation of Testing.SpoecFlow into single solution/repo

---
+++ scr/OneCog.Testing.FakeItEasy/Extensions.cs
@@ -0,0 +1,17 @@
+﻿using FakeItEasy.Configuration;
+using FakeItEasy.Core;
+using System;
+using System.Collections.Generic;
+
+namespace OneCog.Testing.FakeItEasy
+{
+    public static class Extensions
+    {
+        public static IAfterCallSpecifiedWithOutAndRefParametersConfiguration ReturnsInSequence<T>(this IReturnValueArgumentValidationConfiguration<T> source, params Func<IFakeObjectCall, T>[] results)
+        {
+            Stack<Func<IFakeObjectCall, T>> sequence = new Stack<Func<IFakeObjectCall, T>>(results);
+
+            return source.ReturnsLazily(call => sequence.Pop()(call));
+        }
+    }
+}
